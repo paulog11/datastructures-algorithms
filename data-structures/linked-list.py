bfc7b706542d3c2adf60accdf3aad8f0547abc25
PauloG <pmg119@hotmail.com>
2018-06-15 15:17:23 -0700

    linked-list insertion

--- data-structures/linked-list.py
+++ data-structures/linked-list.py
@@ -6,3 +6,26 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+    
+    def insertAtBeginning(self, value):
+        newHead = Node(value)
+        newHead.next = self.head
+        self.head = newHead
+    
+    def printList(self):
+        currNode = self.head
+        while(currNode):
+            print(currNode.data) 
+            currNode = currNode.next
+
+if __name__=='__main__':
+    llist = LinkedList()
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    llist.head.next = second
+    second.next = third
+
+    llist.insertAtBeginning(15)
+    llist.printList()
